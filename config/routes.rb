1472d5d534fc6e5f654cd3ecfdf8797f81e5b89f
Kylian Janssens <kylianjanssens@hotmail.com>
2023-10-01 13:57:41 +0200

    redo my firts rails app

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # verb "path", to: "controller#action"
+  root to: "pages#home"
+
+  get "/about", to: "pages#about"
+
+  get "/contact", to: "pages#contact"
+
 end
